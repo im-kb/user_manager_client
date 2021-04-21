305aae502de0f26016009a5009935ac9446ee067
budzikbudzik <kamil.budzik98@gmail.com>
2021-04-21 14:39:31 +0200

    adding done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,14 @@ import {Component, OnInit} from '@angular/core';
 import {HttpClient, HttpErrorResponse} from '@angular/common/http';
 import {UserService} from './services/user.service';
 import {User} from '../model/User';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent  implements OnInit{
+export class AppComponent implements OnInit {
   public users: User[];
 
   constructor(private userService: UserService) {
@@ -28,6 +29,40 @@ export class AppComponent  implements OnInit{
         alert(error.message);
       }
     );
+  }
+
+  public onAddUser(addForm: NgForm): void {
+    document.getElementById('add-employee-form').click(); //close modal
+    this.userService.addUser(addForm.value).subscribe(
+      (response: User) => {
+        console.log(response);
+        this.getUsers();
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      },
+    );
+  }
+
+  public openModal(user: User, mode: string): void {
+    const container = document.querySelector('.container');
+    const button = document.createElement('button');
+
+    button.type = 'button';
+    button.style.display = 'none';
+
+    button.setAttribute('data-toggle', 'modal');
+
+    if (mode === 'add') {
+      button.setAttribute('data-target', '#addUserModal');
+    } else if (mode === 'edit') {
+      button.setAttribute('data-target', '#updateUserModal');
+    } else {
+      button.setAttribute('data-target', '#deleteUserModal');
+    }
+
+    container.appendChild(button);//add item do dom under div
+    button.click();
   }
 
 
