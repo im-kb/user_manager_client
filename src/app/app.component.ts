4ac26b6af3504b9df2f3a2060e1c55e6c02a397c
budzikbudzik <kamil.budzik98@gmail.com>
2021-04-21 12:20:09 +0200

    display done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient, HttpErrorResponse} from '@angular/common/http';
+import {UserService} from './services/user.service';
+import {User} from '../model/User';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'user-manager-client';
+export class AppComponent  implements OnInit{
+  public users: User[];
+
+  constructor(private userService: UserService) {
+  }
+
+  ngOnInit() {
+    this.getUsers();
+  }
+
+  public getUsers(): void {
+    this.userService.getUsers().subscribe(
+      (response: User[]) => {
+        this.users = response;
+        console.log(this.users);
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+  }
+
+
 }
